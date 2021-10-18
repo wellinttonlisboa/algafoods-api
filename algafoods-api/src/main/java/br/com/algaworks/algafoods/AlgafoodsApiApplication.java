f98fdf62b99265c575953fe1f23abe4b163984ad
wlisb <wlisb@Dragon-PC>
2021-10-18 20:34:25 -0300

    first commit

---
+++ algafoods-api/src/main/java/br/com/algaworks/algafoods/AlgafoodsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.algaworks.algafoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodsApiApplication.class, args);
+	}
+
+}
